ff66ed999d0d6b9f40b4cc9975d948b66052908d
Aathisankar-A <aathisankarayyappan@gmail.com>
2025-09-19 09:36:19 +0530

    Refactor Main and DatabaseConnection classes;

--- src/main/java/com/todo/util/DatabaseConnection.java
+++ src/main/java/com/todo/util/DatabaseConnection.java
@@ -1,25 +1,21 @@
 package com.todo.util;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 public class DatabaseConnection {
-  public static final String URL = "jdbc:mysql://localhost:3306/todo";
-  public static final String USERNAME = "root";
-  public static final String PASSWORD = "aathi##05";
+    public static final String URL = "jdbc:mysql://localhost:3306/todo";
 
-  static{
-    try{
-      Class.forName("com.mysql.cj.jdbc.Driver");
-
+    public static final String USERNAME="root";
+    public static final String PASSWORD="aathi##05";
+    static{
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch(ClassNotFoundException e){
+            System.out.println("Error: Unable to load database driver.");
+        }
     }
-    catch(ClassNotFoundException e){
-      System.out.println("JDBC Driver is missing");
+    public  Connection getDBConnection() throws SQLException{
+        return DriverManager.getConnection(URL,USERNAME,PASSWORD);
     }
-  }
-
-  public Connection getDBConnection() throws SQLException{
-    return DriverManager.getConnection(URL, USERNAME, PASSWORD);
-  }
 }
